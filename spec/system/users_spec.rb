adaf524660474fab85116aba69622a6505358762
yasuka133 <t_asukaasuka@yahoo.co.jp>
2026-03-23 16:43:24 +0900

    結合テスト2

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -13,6 +13,25 @@ RSpec.describe "Users", type: :system do
       expect(page).to have_content('ログイン')
     end
 
+    it '新規登録ボタンをクリックすると新規登録ページに遷移する' do
+      # トップページに遷移する
+      visit root_path
+      # 新規登録ボタンをクリックする
+      click_on '新規登録'
+      # 新規登録ページに遷移したことを確認する
+      expect(page).to have_current_path(new_user_registration_path)
+    end
+
+    it 'ログインボタンをクリックするとログインページに遷移する' do
+      # トップページに遷移する
+      visit root_path
+      # ログインボタンをクリックする
+      click_on 'ログイン'
+      # ログインページに遷移したことを確認する
+      expect(page).to have_current_path(new_user_session_path)
+    end
+
+
    context 'ログイン状態のとき' do
     it 'トップページに「ニックネーム」と「ログアウト」ボタンが表示される' do
       # 事前にユーザーをDBに保存
@@ -38,6 +57,29 @@ RSpec.describe "Users", type: :system do
       # 新規登録とログインボタンがないことを確認
       expect(page).to have_no_content('新規登録')
       expect(page).to have_no_content('ログイン')
+    end
+   
+    it 'ログアウトボタンをクリックするとログアウトができる' do
+       @user.save
+      # ログインする
+      visit new_user_session_path
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      find('input[name="commit"]').click
+      expect(page).to have_current_path(root_path)
+
+      # ログアウトボタンをクリックする
+      click_on 'ログアウト'
+
+      # トップページに戻ってきたことを確認する
+      expect(page).to have_current_path(root_path)
+
+      # 画面内に「新規登録」「ログイン」ボタンが表示されていることを確認する
+      expect(page).to have_content('新規登録')
+      expect(page).to have_content('ログイン')
+
+      # 画面内に「ログアウト」ボタンが表示されていないことを確認する
+      expect(page).to have_no_content('ログアウト')
     end
   end
   end
