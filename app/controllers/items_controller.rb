66957c8a951bc05d84411e51ab4b250c9d18c708
yasuka133 <t_asukaasuka@yahoo.co.jp>
2026-03-31 17:56:16 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,9 +18,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-    @item = Item.find(params[:id])
-  end
+  #def show
+    #@item = Item.find(params[:id])
+  #end
 
   private
 
