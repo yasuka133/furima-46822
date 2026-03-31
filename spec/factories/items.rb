9aabef55088aac0b425be952eb0a5fbd13340a78
yasuka133 <t_asukaasuka@yahoo.co.jp>
2026-03-31 11:03:39 +0900

    テストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :item do
-    item_name { "MyString" }
-    item_info { "MyText" }
-    item_category_id { 1 }
-    item_sales_status_id { 1 }
-    item_shipping_fee_status_id { 1 }
-    item_prefecture_id { 1 }
-    item_scheduled_delivery_id { 1 }
-    item_price { 1 }
-    user { nil }
+    item_name                  { "商品名" }
+    item_info                  { "商品の説明" }
+    item_category_id           { 2 } # 1以外
+    item_sales_status_id       { 2 }
+    item_shipping_fee_status_id { 2 }
+    item_prefecture_id         { 2 }
+    item_scheduled_delivery_id  { 2 }
+    item_price                 { 1000 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '出品できるとき' do
+      it '全ての項目が存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品できないとき' do
+      it '画像が空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("画像を入力してください")
+      end
+
+      it '価格が300円未満では登録できない' do
+        @item.item_price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("販売価格は300以上の値にしてください")
+      end
+
+      it 'カテゴリーで「---」(id:1)を選択すると登録できない' do
+        @item.item_category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("カテゴリーを選択してください")
+      end
+      
+      it '価格に半角数字以外が含まれている場合は登録できない' do
+        @item.item_price = "１０００" # 全角
+        @item.valid?
+        expect(@item.errors.full_messages).to include("販売価格は半角数値で入力してください")
+      end
+    end
+  end
 end
