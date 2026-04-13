4098a92e1cd2051661e4eeccca85e781208b6dcf
yasuka133 <t_asukaasuka@yahoo.co.jp>
2026-04-13 18:40:25 +0900

    テストコード記述

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,19 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'factory_bot'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::IntegrationHelpers, type: :system
+end
+
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
