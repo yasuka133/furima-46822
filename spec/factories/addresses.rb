1d11f9dde85e21cdb540242a396a2fc22fbd2b75
yasuka133 <t_asukaasuka@yahoo.co.jp>
2026-04-23 10:42:48 +0900

    マイグレーションファイル作成

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :address do
+    postal_code { "MyString" }
+    prefecture_id { 1 }
+    city { "MyString" }
+    house_number { "MyString" }
+    building { "MyString" }
+    phone_number { "MyString" }
+    order { nil }
+  end
+end
